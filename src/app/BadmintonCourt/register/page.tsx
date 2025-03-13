aabc31d0b001a9e950e471f0055a3b74bad38070
wuttipong098 <64020800@up.ac.th>
2025-03-13 07:03:49 +0700

    feature/Headersuccess-login-new

---
+++ src/app/BadmintonCourt/register/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "next/image";
+import ball from "@/public/ball.png";
+import court from "@/public/corut.png";
+
+const RegisterPage = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default RegisterPage;
