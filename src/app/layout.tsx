5efb812d826a45f9e35fd7132aa6099a716c35b0
AekawitP <darmkubz@gmail.com>
2025-03-14 19:12:45 +0700

    ีupdate from main to dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import '../styles/globals.css';
 import Header from './BadmintonCourt/components/Header';
 import { usePathname } from 'next/navigation';
-
 import HeaderShop from './BadmintonShop/components/Header';
 export default function RootLayout({
     children,
@@ -14,8 +13,9 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                {pathname !== '/BadmintonCourt/login' && pathname !== '/BadmintonCourt/register' && <Header />}
-            {pathname.includes("/BadmintonShop") ? <HeaderShop /> : <Header />}
+                {!pathname.includes("/BadmintonCourt/login") &&
+                 !pathname.includes("/BadmintonCourt/register") &&
+                 (pathname.includes("/BadmintonShop") ? <HeaderShop /> : <Header />)}
                 <main>{children}</main>
             </body>
         </html>
